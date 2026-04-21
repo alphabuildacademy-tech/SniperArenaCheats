65de13b60a77e53fcaf0ded90b236824fab2e6d0
ZoroDev_World <alphabuild.academy@gmail.com>
2026-04-21 12:28:14 -0400

    Updated Sniper Arena

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 -- loader.lua
 -- Sniper Arena Loader
-loadstring(game:HttpGet("https://raw.githubusercontent.com/alphabuildacademy-tech/sniper-arena-cheat/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alphabuildacademy-tech/SniperArenaCheats/main/main.lua"))()
